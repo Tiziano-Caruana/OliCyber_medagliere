3f9af00012958093f47035f8b2461e0f4e2997af
Marco <marco.pellero.it@gmail.com>
2023-07-30 22:40:24 +0200

    way to fetch editions data without a webdriver

--- scraper/main.py
+++ scraper/main.py
@@ -4,6 +4,39 @@ from bs4 import BeautifulSoup
 from requests import *
 from selenium import webdriver
 from selenium.webdriver.chromium.options import *
+import requests
+import json
+
+def get_all_editions_data():
+    """
+    the data we want is hardcoded in a JS file with name `main-es2015.{hex stuff}.js`
+    i think that hex stuff is an id which changes on site build, so i'm extracting it from the page html
+    """
+
+    # get the site's homepage
+    homepage_url = "https://olicyber.it/edizioni/2021" # it loads data for all editions no matter what edition you lookup
+    resp = requests.get(homepage_url)
+    html = resp.text
+    
+    # find the src of the target script
+    soup = BeautifulSoup(html, "html.parser")
+    scripts = soup.select("script")
+    target_script = next(filter(lambda tag: tag.attrs["src"].startswith("main"), scripts))
+
+    # get the script itself
+    script_url = "https://olicyber.it/" + target_script.attrs["src"]
+    resp = requests.get(script_url)
+    code = resp.text
+
+    # it's 1 object with data for all editions, in order. the first one was in 2021
+    left = code.index('{"2021":{"scolastica')
+    right = code.index("')", left)
+    data = code[left:right]
+
+    # remove escapes like \\ or \x or \'
+    data = data.encode().decode("unicode_escape")
+
+    return json.loads(data)
 
 # Get da whole thing
 N = [136, 95]
